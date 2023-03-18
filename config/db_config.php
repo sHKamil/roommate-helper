348f73a6c78b083047706cf31cd8ca75380ca1ab
unknown <shootekb@gmail.com>
2023-03-18 15:06:32 +0100

    Clearing database config

--- config/db_config.php
+++ config/db_config.php
@@ -1,8 +1,8 @@
 <?php
 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'admin');
-    define('DB_NAME', 'rhelper');
+    define('DB_HOST', 'localhost'); // Fill it with your details
+    define('DB_USER', ''); // Database user
+    define('DB_PASS', ''); // Database password
+    define('DB_NAME', ''); // Database name (schema name)
 
 ?>
