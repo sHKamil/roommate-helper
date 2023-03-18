bbd4e904a49780939727ea14c9089ec48b6df10f
unknown <shootekb@gmail.com>
2023-03-18 15:15:13 +0100

    Git exclude instead of ignore

---
+++ config/db_config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('DB_HOST', 'localhost'); // Fill it with your details
+    define('DB_USER', ''); // Database user
+    define('DB_PASS', ''); // Database password
+    define('DB_NAME', ''); // Database name (schema name)
+
+?>
