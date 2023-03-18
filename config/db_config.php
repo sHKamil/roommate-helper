4d6c2138387cf33a3bd3f353275af58e155c74e8
unknown <shootekb@gmail.com>
2023-03-18 15:12:36 +0100

    Git ignore test

--- config/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define('DB_HOST', 'localhost'); // Fill it with your details
-    define('DB_USER', ''); // Database user
-    define('DB_PASS', ''); // Database password
-    define('DB_NAME', ''); // Database name (schema name)
-
-?>
