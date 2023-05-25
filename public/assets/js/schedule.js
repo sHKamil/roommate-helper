2fd4a69359cb2802a73fb56aaca9bdbdb5acc8f7
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-05-25 15:56:23 +0200

    bootstrap moved to public dir

---
+++ public/assets/js/schedule.js
@@ -0,0 +1,38 @@
+var date = new Date();
+var ad = $('#actual_date');
+var day = date.getDate();
+ad.text(date.getDate()+"."+date.getMonth()+"."+date.getFullYear());
+
+console.log(getCookie("today"));
+
+setDay(getCookie("today"));
+
+function getCookie(name) {
+  let pattern = RegExp(name + "=[^;]*");
+  const matched = document.cookie.match(pattern);
+
+  if (matched) {
+    const cookie = matched[0].split('=');
+    let string = cookie[1];
+    pattern = RegExp(/[%22]*/g);
+    string = string.replace(pattern,'');
+    return string;
+  }
+  return null;
+}
+
+function setDay(day) {
+  clearActualDay()
+  const element = document.getElementById('d'+day);
+  element.classList.add('actual_day');
+}
+
+function clearActualDay() {
+  const elements = document.querySelectorAll('.actual_day');
+
+  elements.forEach((element) => {
+    element.classList.remove('actual_day');
+  });
+}
+
+
