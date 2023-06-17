e44fb6ec4c562e21d5d5825d689539b43e9b52d4
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-06-17 17:36:38 +0200

    IP Security update

--- app/Middleware/Member.php
+++ app/Middleware/Member.php
@@ -2,14 +2,26 @@
 
 namespace app\Middleware;
 
+use app\Services\Validator;
+
 class Member
 {
     public function handle()
     {
         if(!$_SESSION['user_type'] ?? false)
         {
-            header('Location: /');
-            exit();
+            $this->_logout();
         }
+
+        if(!Validator::sessionClientIP()) {
+            $this->_logout();
+        }
+    }
+
+    private function _logout()
+    {
+        session_destroy();
+        header('Location: /login');
+        exit();
     }
 }
