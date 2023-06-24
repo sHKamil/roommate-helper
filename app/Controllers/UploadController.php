b21de045156811486e16524b6b01e2f28e9b59f4
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-06-24 23:24:24 +0200

    UploadController

---
+++ app/Controllers/UploadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\Controllers;
+
+use app\Controllers\EditionController;
+use app\Models\User;
+use app\Service\Alert;
+use app\Services\Validator;
+
+class UploadController
+{
+    private $img;
+    private $img_name;
+    private $destination_img;
+    private $user;
+    public $errors;
+
+}
