aaab5c69b1b349baa3795fc4ea7a882d502a64da
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-07-05 09:03:15 +0200

    AbstractModel added

---
+++ app/Abstract/AbstractModel.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\Abstract;
+
+use app\Database\DatabasePDO;
+use app\Services\ModelHandler;
+
+abstract class AbstractModel
+{
+    private $db;
+    public $allColumns = [];
+    public $fillable = [];
+    public $table;
+        
+    public function add(array $params = []) : bool
+    {
+        $fillable = $this->fillable;
+        $columns = ModelHandler::prepareFillableForSQL($fillable);
+        $placeholders = ModelHandler::preparePlaceholders($fillable);
+        try
+        {
+            if($this->db->query("INSERT INTO " . $this->table . "(" . $columns . ") VALUES (" . $placeholders . ")", $params) !== false) return true;
+        } catch (\Exception $e) {
+            echo "Insert query failed: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function updateById(array $params = [], string $columns) : bool
+    {
+        try
+        {
+
+            if($this->db->query("UPDATE $this->table SET $columns WHERE id = :id", $params) !== false) {
+                return true;
+            };
+        } catch (\PDOException $e) {
+            echo "Insert query failed: " . $e->getMessage();
+        }
+        return false;
+    }
+
+    public function getByWhere(array $params = [],string $select, string $where) : array | false
+    {
+        try
+        {
+            $result = $this->db->query("SELECT $select FROM " . $this->table . " WHERE $where", $params)->fetch();
+            return $result;
+        } catch (\PDOException $e) {
+            echo "Insert query failed: " . $e->getMessage();
+        }
+        return false;
+    }
+
+    public function deleteById(array $params = []) : bool
+    {
+        $query = "DELETE FROM " . $this->table . " WHERE id = :id;";
+        try
+        {
+            if($this->db->query($query, $params) !== false) return true;
+        } catch (\PDOException $e) {
+            echo "Delete query failed: " . $e->getMessage();
+            return false;
+        }
+    }
+}
