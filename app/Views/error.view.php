a810f34b54b2a0c9677c930ed191949cca17b22e
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-07-25 12:00:54 +0200

    error view update

--- app/Views/error.view.php
+++ app/Views/error.view.php
@@ -1,14 +1,18 @@
 <?php
 
-
-
 use app\Services\HtmlFactory;
 
-echo HtmlFactory::buildHeader("Rhelper - LogIn");
-
+echo HtmlFactory::buildHeader("Rhelper - Error");
 ?>
-
-ERROR 404: Page not found! <br>
-Go back to <a href="/">home page.</a>
-
+ <div class="nav-component">
+    <a href="/">
+        <div class="logo">
+            <img src="/assets/images/logo.png" alt="Logo" width="350px">
+        </div>
+    </a>
+</div>
+<div class="component" style="margin: 5vh 5vw; display: block; text-align:center;">
+        <h1 style="font-size: 5rem;">ERROR 404: Page not found! </h1><br>
+        <h2 style="font-size: 5rem;">Go back to <a href="/">home page.</a></h2>
+</div>
 <?php echo HtmlFactory::buildFooter(); ?>
