b211678199bb7bf865f970f106f465c99fdc5ddc
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-07-29 19:48:49 +0200

    Edit styles changes

--- app/Views/event-edit.view.php
+++ app/Views/event-edit.view.php
@@ -7,21 +7,19 @@ echo HtmlFactory::buildNav();
 ?>
 
 <div class="container">
-	<div class="component-pin">
-        <div class="info">
-            <p>Last edit by: <?php echo $last_user ?> </p>
-            <p>Event ID: <?php echo $data['id'] ?> </p>
-        </div>
-    </div>
 	<div class="component-form">
 		<form action="" method="post">
+            <div class="info">
+                <p>Last edit by: <?php echo $last_user ?> </p>
+                <p>Event ID: <?php echo $data['id'] ?> </p>
+            </div>
             <input type="hidden" name="_method" value="PATCH">
             <input type="hidden" name="id" value=<?php echo $data['id'] ?>>
-            <input class="form__input" type="text" name="event_name" placeholder="Event name" value=<?php echo $data['event_name'] ?> required>
-            <input class="form__input" type="text" name="content" placeholder="Content" value=<?php echo $data['content'] ?> required>
-            <input class="form__input" type="date" name="day" placeholder="Day" value=<?php echo $data['day'] ?> required>
-            <input class="form__input" type="time" name="start" placeholder="Start" value=<?php echo $data['start'] ?> required>
-            <input class="form__input" type="time" name="end" placeholder="End" value=<?php echo $data['end'] ?> required>
+            <div class="input-with-name"><input class="form__input" type="text" name="event_name" placeholder="Event name" value=<?php echo $data['event_name'] ?> required> Event name </div>
+            <div class="input-with-name"><input class="form__input" type="text" name="content" placeholder="Content" value=<?php echo $data['content'] ?> required> Content </div>
+            <div class="input-with-name"><input class="form__input" type="date" name="day" placeholder="Day" value=<?php echo $data['day'] ?> required> Day </div>
+            <div class="input-with-name"><input class="form__input" type="time" name="start" placeholder="Start" value=<?php echo $data['start'] ?> required> Start </div>
+            <div class="input-with-name"><input class="form__input" type="time" name="end" placeholder="End" value=<?php echo $data['end'] ?> required> End </div>
             <input  type="submit" class="btns btn__primary" value="Save">
             
             <a href="event" class="abtn" >
