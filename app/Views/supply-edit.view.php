e454f2cd277e1dbf3a6ddc132631806c8b2f816b
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-07-31 15:25:55 +0200

    supply edit style changes

--- app/Views/supply-edit.view.php
+++ app/Views/supply-edit.view.php
@@ -7,22 +7,20 @@ echo HtmlFactory::buildNav();
 ?>
 
 <div class="container">
-	<div class="component-pin">
-		<div class="info">
-			<p>Last edit by: <?php echo $last_user ?> </p>
-			<p>Expected end: <?php echo $data['expected_end'] ?> </p>
-			<p>Supply ID: <?php echo $data['id'] ?> </p>
-		</div>
-	</div>
 	<div class="component-form">
 		<form action="" method="post">
-            
+            <div class="form_title">Supply</div>
+            <div class="info">
+                <p>Last edit by: <?php echo $last_user ?> </p>
+                <p>Expected end: <?php echo $data['expected_end'] ?> </p>
+                <p>Supply ID: <?php echo $data['id'] ?> </p>
+            </div>
             <input type="hidden" name="_method" value="PATCH">
             <input type="hidden" name="id" value=<?php echo $data['id'] ?>>
-            <input class="form__input" type="text" name="name" placeholder="Name" value=<?php echo $data['name'] ?> required>
-            <input class="form__input" type="text" name="quantity_max" placeholder="Max quantity" value=<?php echo $data['quantity_max'] ?> required>
-            <input class="form__input" type="text" name="quantity" placeholder="Actual quantity" value=<?php echo $data['quantity'] ?> required>
-            <input class="form__input" type="text" name="days_until_ends" placeholder="Days until ends" required>
+            <div class="input-with-name"><input class="form__input" type="text" name="name" placeholder="Name" value=<?php echo $data['name'] ?> required> Suppy name </div>
+            <div class="input-with-name"><input class="form__input" type="text" name="quantity_max" placeholder="Max quantity" value=<?php echo $data['quantity_max'] ?> required> Max quantity </div>
+            <div class="input-with-name"><input class="form__input" type="text" name="quantity" placeholder="Actual quantity" value=<?php echo $data['quantity'] ?> required> Actual quantity </div>
+            <div class="input-with-name"><input class="form__input" type="text" name="days_until_ends" placeholder="Days until ends" required> Days until ends </div>
             <input  type="submit" class="btns btn__primary">
             
             <a href="supply" class="abtn" >
