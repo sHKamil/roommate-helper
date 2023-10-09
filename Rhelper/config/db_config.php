8e4e3aebaf14cc063246a3eca2e0257f72d68b3d
sHKamil <shootekb@gmail.com>
2023-10-09 16:33:26 +0200

    config - added

---
+++ Rhelper/config/db_config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('DB_HOST', 'mariadb'); // Fill it with your details
+    define('DB_PORT', '3306'); // Fill it with your details
+    define('DB_USER', 'root'); // Database user
+    define('DB_PASS', 'pass'); // Database password
+    define('DB_NAME', 'rhelper'); // Database name (schema name)
+    define('DB_CHARSET', 'utf8mb4'); // Fill it with your details
